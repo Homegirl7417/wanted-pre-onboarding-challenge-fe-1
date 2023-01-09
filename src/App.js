d303d6801f8c3a73cdaa199310396298f11170bd
ParkHyunKyeong <kj3477417@gmail.com>
2023-01-09 22:01:29 +0900

    feature: 리액트 기본 설정 파일 중 미사용 파일 제거

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
